6821834d36e7490caed8d9293b6c231589274536
CrazyCakes <291845444@qq.com>
2019-09-02 15:41:28 +0800

    建立算法类

---
+++ MedicalVisualization/CAlgorithm.h
@@ -0,0 +1,20 @@
+#pragma once
+class CAlgorithm
+{
+public:
+	CAlgorithm();
+	~CAlgorithm();
+
+//  K阶领域显示
+	void KOrderDomain();
+
+// 叶子节点显示
+	void ShowLeafNodes();
+
+// 三维重建
+	void ThreeDimensionalReconstruction();
+
+// 孔洞修补
+	void HoleRepair();
+};
+
